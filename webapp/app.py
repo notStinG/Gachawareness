0b87442ba142171c6374066f621dd10671abd7ae
Graldo Coil <gerrarddada@gmail.com>
2024-10-16 12:24:29 +0800

    Your commit message

--- webapp/app.py
+++ webapp/app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, redirect, request, url_for
 
-
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return url_for("index")
+    return render_template('index.html')
+
+@app.route("/gambling-behaviour")
+def gambling_behaviour():
+    return render_template('gambling-behaviour.html')
+
+@app.route("/financial-exploitations")
+def financial_exploitations():
+    return render_template('financial-exploitations.html')
+
+@app.route("/mental-health")
+def mental_health():
+    return render_template('mental-health.html')
 
 if __name__ == "__main__":
-    app.run(port=1, debug=True)
+    app.run(port=5000, debug=True)
